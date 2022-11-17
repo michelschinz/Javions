7ad1bc9f32328ab887fc4c72358e3586caf6658e
Michel Schinz <michel@schi.nz>
2022-11-17 10:47:48 +0100

    Validate lon/lat angles given to GeoPos

--- src/ch/epfl/javions/GeoPos.java
+++ src/ch/epfl/javions/GeoPos.java
@@ -3,12 +3,14 @@ package ch.epfl.javions;
 public record GeoPos(int intLon, int intLat) {
     private static final int INT_90_DEGREES = 1 << 30;
 
-    public GeoPos {
-        Preconditions.checkArgument(-INT_90_DEGREES <= intLat && intLat <= INT_90_DEGREES);
+    private static double checkLon(double lon) {
+        Preconditions.checkArgument(-Math.PI <= lon && lon < Math.PI);
+        return lon;
     }
 
-    public GeoPos(double lon, double lat) {
-        this(encode(lon), encode(lat));
+    private static double checkLat(double lat) {
+        Preconditions.checkArgument(-Math.PI / 2 <= lat && lat <= Math.PI / 2);
+        return lat;
     }
 
     private static int encode(double angle) {
@@ -19,6 +21,14 @@ public record GeoPos(int intLon, int intLat) {
         // Warning: the cast is necessary to call the correct variant of scalb
         //   and avoid losing precision.
         return Math.scalb((double) angle, -Integer.SIZE) * Units.Angle.TURN;
+    }
+
+    public GeoPos {
+        Preconditions.checkArgument(-INT_90_DEGREES <= intLat && intLat <= INT_90_DEGREES);
+    }
+
+    public GeoPos(double lon, double lat) {
+        this(encode(checkLon(lon)), encode(checkLat(lat)));
     }
 
     public double longitude() {
