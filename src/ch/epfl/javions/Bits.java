c8a669f084ca7c21c77ebc056df3de4d0623e390
Michel Schinz <michel@schi.nz>
2023-01-03 12:44:50 +0100

    Delete (unused) Bits.extractSInt

--- src/ch/epfl/javions/Bits.java
+++ src/ch/epfl/javions/Bits.java
@@ -10,15 +10,6 @@ public final class Bits {
         return (int) ((value << (unusedBits - startBit)) >>> unusedBits);
     }
 
-    public static int extractSInt(long value, int startBit, int length) {
-        // TODO add more preconditions
-        //  (in particular, disallow length == 0, as the code below probably doesn't work then).
-        Preconditions.checkArgument(length < Integer.SIZE);
-
-        var unusedBits = Long.SIZE - length;
-        return (int) ((value << (unusedBits - startBit)) >> unusedBits);
-    }
-
     public static int extractBit(long value, int bit) {
         return (int) ((value >> bit) & 1);
     }
