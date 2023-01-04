2a634007ecad7b9b330e1cf920ca842cd9a177c5
Michel Schinz <michel@schi.nz>
2023-01-04 11:02:35 +0100

    Remove unnecessary MessageType enum

--- src/ch/epfl/javions/adsb/Message.java
+++ src/ch/epfl/javions/adsb/Message.java
@@ -27,23 +27,6 @@ public sealed interface Message permits AirbornePositionMessage, AirborneVelocit
         return message.bytesBetween(4, 11);
     }
 
-    static MessageType typeCode(int rawTypeCode) {
-        return switch (rawTypeCode) {
-            case 1, 2, 3, 4 -> MessageType.AIRCRAFT_IDENTIFICATION;
-            case 5, 6, 7, 8 -> MessageType.SURFACE_POSITION;
-            case 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22 -> MessageType.AIRBORNE_POSITION;
-            case 19 -> MessageType.AIRBORNE_VELOCITIES;
-            case 28 -> MessageType.AIRCRAFT_STATUS;
-            case 29 -> MessageType.TARGET_STATE;
-            case 31 -> MessageType.AIRCRAFT_OPERATION_STATUS;
-            default -> MessageType.UNKNOWN;
-        };
-    }
-
-    static MessageType typeCode(ByteString msg) {
-        return typeCode(rawTypeCode(msg));
-    }
-
     static int rawIcaoAddress(ByteString msg) {
         return (int) msg.bytesBetween(1, 4);
     }
@@ -55,10 +38,10 @@ public sealed interface Message permits AirbornePositionMessage, AirborneVelocit
     static Message of(long timeStamp, ByteString bytes) {
         if (rawDownLinkFormat(bytes) != 17) return null; // FIXME clean-up
 
-        return switch (typeCode(bytes)) {
-            case AIRCRAFT_IDENTIFICATION -> AircraftIdentificationMessage.of(timeStamp, bytes);
-            case AIRBORNE_VELOCITIES -> AirborneVelocityMessage.of(timeStamp, bytes);
-            case AIRBORNE_POSITION -> AirbornePositionMessage.of(timeStamp, bytes);
+        return switch (rawTypeCode(bytes)) {
+            case 1, 2, 3, 4 -> AircraftIdentificationMessage.of(timeStamp, bytes);
+            case 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22 -> AirbornePositionMessage.of(timeStamp, bytes);
+            case 19 -> AirborneVelocityMessage.of(timeStamp, bytes);
             default -> null;
         };
     }
