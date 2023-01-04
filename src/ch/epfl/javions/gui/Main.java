f491a079841d0dfe4d66c74b26b6fed7ba60ee31
Michel Schinz <michel@schi.nz>
2023-01-04 10:59:41 +0100

    Remove unused imports in Main

--- src/ch/epfl/javions/gui/Main.java
+++ src/ch/epfl/javions/gui/Main.java
@@ -17,8 +17,6 @@ import java.io.EOFException;
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
