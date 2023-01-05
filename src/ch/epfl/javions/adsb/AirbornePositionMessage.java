b4d7bcdac12a60407f110a3d2be46f2f142627f7
Michel Schinz <michel@schi.nz>
2023-01-05 17:01:13 +0100

    Decode Gillham-encoded altitudes

--- src/ch/epfl/javions/adsb/AirbornePositionMessage.java
+++ src/ch/epfl/javions/adsb/AirbornePositionMessage.java
@@ -30,19 +30,48 @@ public record AirbornePositionMessage(
     private static final int ALTITUDE_Q_BIT_INDEX = 4;
     private static final int ALTITUDE_Q_BIT_UPPER_MASK = ~0 << (ALTITUDE_Q_BIT_INDEX + 1);
     private static final int ALTITUDE_Q_BIT_LOWER_MASK = (1 << ALTITUDE_Q_BIT_INDEX) - 1;
-    private static final double ALTITUDE_UNIT = 25 * Units.Distance.FOOT;
-    private static final double ALTITUDE_ORIGIN = -1000 * Units.Distance.FOOT;
 
     private static double altitude(long payload) {
         var encAltitude = UNPACKER.unpack(Field.ALT, payload);
         if (Bits.testBit(encAltitude, ALTITUDE_Q_BIT_INDEX)) {
-            var altitude = (encAltitude & ALTITUDE_Q_BIT_UPPER_MASK) >> 1
+            var ft25 = (encAltitude & ALTITUDE_Q_BIT_UPPER_MASK) >> 1
                     | (encAltitude & ALTITUDE_Q_BIT_LOWER_MASK);
-            return ALTITUDE_ORIGIN + altitude * ALTITUDE_UNIT;
+            return (-1000 + ft25 * 25) * Units.Distance.FOOT;
         } else {
-            // FIXME implement (see https://www.wikiwand.com/en/Gillham_code and http://www.ccsinfo.com/forum/viewtopic.php?p=140960#140960)
-            return Double.NaN;
+            return decodeGillhamAltitude(permuteGillham(encAltitude));
         }
+    }
+
+    //         11 10  9  8  7  6  5  4  3  2  1  0
+    //  Input: C1 A1 C2 A2 C4 A4 B1 D1 B2 D2 B4 D4
+    // Output: D1 D2 D4 A1 A2 A4 B1 B2 B4 C1 C2 C4
+    static int permuteGillham(int shuffled) {
+        var unshuffled = 0;
+        for (var i : new int[] {4, 10, 5, 11}) {
+            for (var j = 0; j < 5; j += 2) {
+                var bit = (shuffled >> (i - j)) & 1;
+                unshuffled = (unshuffled << 1) | bit;
+            }
+        }
+        return unshuffled;
+    }
+
+    static double decodeGillhamAltitude(int encAltitude) {
+        // Algorithm taken from http://www.ccsinfo.com/forum/viewtopic.php?p=140960#140960
+        var ft100 = gray16ToBinary(Bits.extractUInt(encAltitude, 0, 3));
+        if (ft100 == 0 || ft100 == 5 || ft100 == 6) return Double.NaN;
+        if (ft100 == 7) ft100 = 5;
+
+        var ft500 = gray16ToBinary(Bits.extractUInt(encAltitude, 3, 9));
+        if ((ft500 & 1) == 1) ft100 = 6 - ft100;
+        return (-1300 + ft100 * 100 + ft500 * 500) * Units.Distance.FOOT;
+    }
+
+    private static int gray16ToBinary(int gray) {
+        assert 0 <= gray && gray < (1 << 16);
+        var binary = gray;
+        for (var i = 8; i > 0; i >>= 1) binary ^= binary >> i;
+        return binary;
     }
 
     public static AirbornePositionMessage of(long timeStamp, ByteString bytes) {
