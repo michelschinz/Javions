23e669e760208edef0ff13685b1f2e50b63fdd61
Michel Schinz <michel@schi.nz>
2023-01-06 14:38:04 +0100

    Inline SampleBytesReader into SamplesDecoder

--- src/ch/epfl/javions/demodulation/SamplesDecoder.java
+++ src/ch/epfl/javions/demodulation/SamplesDecoder.java
@@ -1,28 +1,37 @@
 package ch.epfl.javions.demodulation;
 
+import ch.epfl.javions.Preconditions;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 public final class SamplesDecoder {
     private final int chunkSize;
     private final byte[] byteChunk;
-    private final SampleBytesReader bytesReader;
+    private final InputStream stream;
 
     public SamplesDecoder(InputStream stream, int chunkSize) {
+        Preconditions.checkArgument(chunkSize > 0);
+
         var byteChunkSize = chunkSize * Short.BYTES;
         this.chunkSize = chunkSize;
         this.byteChunk = new byte[byteChunkSize];
-        this.bytesReader = new SampleBytesReader(stream, byteChunkSize);
+        this.stream = stream;
     }
 
     public int readChunk(short[] chunk) throws IOException {
-        assert chunk.length == chunkSize;
-        var bytesRead = bytesReader.readChunk(byteChunk);
-        for (var i = 0; i < chunkSize; i += 1) {
+        Preconditions.checkArgument(chunk.length == chunkSize);
+
+        var bytesRead = stream.readNBytes(byteChunk, 0, byteChunk.length);
+        var samplesRead = bytesRead / Short.BYTES;
+
+        for (var i = 0; i < samplesRead; i += 1) {
             var lsb = Byte.toUnsignedInt(byteChunk[i * Short.BYTES]);
             var msb = Byte.toUnsignedInt(byteChunk[i * Short.BYTES + 1]);
             chunk[i] = (short) ((msb << Byte.SIZE | lsb) - (1 << 11));
         }
-        return bytesRead / Short.BYTES;
+
+        return samplesRead;
     }
 }
