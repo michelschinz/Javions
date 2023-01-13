2e198b045fc50334c4a1edf250d83488510e187c
Michel Schinz <michel@schi.nz>
2023-01-13 09:16:52 +0100

    Avoid using streams in AircraftDatabase
    Also, fix the bug that, most of the time, some data was returned even
    if the given address did not exist in the database.

--- src/ch/epfl/javions/aircraft/AircraftDatabase.java
+++ src/ch/epfl/javions/aircraft/AircraftDatabase.java
@@ -8,17 +8,6 @@ import java.util.zip.ZipFile;
 public final class AircraftDatabase {
     private static final String SEPARATOR = Pattern.quote(",");
 
-    // Format: ICAO,Registration,Designator,Model,Description,WTC,Flags
-    private static AircraftData parseLine(String line) {
-        var columns = line.split(SEPARATOR);
-        return new AircraftData(
-                new AircraftRegistration(columns[1]),
-                new AircraftTypeDesignator(columns[2]),
-                columns[3],
-                new AircraftDescription(columns[4]),
-                WakeTurbulenceCategory.of(columns[5]));
-    }
-
     private final File dbFile;
 
     public AircraftDatabase(File dbFile) {
@@ -28,13 +17,26 @@ public final class AircraftDatabase {
     public AircraftData get(IcaoAddress address) {
         try {
             try (var zipFile = new ZipFile(dbFile);
-                 var entryStream = zipFile.getInputStream(zipFile.getEntry(entryName(address)))) {
-                var reader = new BufferedReader(new InputStreamReader(entryStream, StandardCharsets.UTF_8));
-                return reader.lines()
-                        .dropWhile(l -> address.toString().compareTo(l) > 0)
-                        .findFirst()
-                        .map(AircraftDatabase::parseLine)
-                        .orElse(null);
+                 var entryStream = zipFile.getInputStream(zipFile.getEntry(entryName(address)));
+                 var reader = new BufferedReader(new InputStreamReader(entryStream, StandardCharsets.UTF_8))) {
+
+                var addressString = address.toString();
+                while (true) {
+                    var line = reader.readLine();
+                    if (line == null) return null;
+                    if (line.compareTo(addressString) < 0) continue;
+                    if (!line.startsWith(addressString)) return null;
+
+                    // Format: ICAO,Registration,Designator,Model,Description,WTC,Flags
+                    var columns = line.split(SEPARATOR);
+                    assert columns[0].equals(addressString);
+                    return new AircraftData(
+                            new AircraftRegistration(columns[1]),
+                            new AircraftTypeDesignator(columns[2]),
+                            columns[3],
+                            new AircraftDescription(columns[4]),
+                            WakeTurbulenceCategory.of(columns[5]));
+                }
             }
         } catch (IOException e) {
             throw new UncheckedIOException(e);
