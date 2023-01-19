a7c88b7f27f0ea09ebe68c09823a15735b93a0d9
Michel Schinz <michel@schi.nz>
2023-01-19 15:22:49 +0100

    Simplify formulas in WebMercator
    Avoid PI.

--- src/ch/epfl/javions/WebMercator.java
+++ src/ch/epfl/javions/WebMercator.java
@@ -1,17 +1,18 @@
 package ch.epfl.javions;
 
 import static ch.epfl.javions.Math2.asinh;
-import static java.lang.Math.*;
+import static java.lang.Math.scalb;
+import static java.lang.Math.tan;
 
 public final class WebMercator {
     private WebMercator() {}
 
     public static double x(int zoomLevel, double lon) {
-        return scalb((lon + PI) * (1d / Units.Angle.TURN), shiftFor(zoomLevel));
+        return scalb(lon * (1d / Units.Angle.TURN) + 0.5, shiftFor(zoomLevel));
     }
 
     public static double y(int zoomLevel, double lat) {
-        return scalb((PI - asinh(tan(lat))) * (1d / Units.Angle.TURN), shiftFor(zoomLevel));
+        return scalb(0.5 - asinh(tan(lat)) * (1d / Units.Angle.TURN), shiftFor(zoomLevel));
     }
 
     private static int shiftFor(int zoomLevel) {
