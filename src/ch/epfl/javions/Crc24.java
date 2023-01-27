4163cc9ade352eab3313d779753994bb70201251
Michel Schinz <michel@schi.nz>
2023-01-27 10:12:12 +0100

    Clean-up Crc24 (name constants, etc.)

--- src/ch/epfl/javions/Crc24.java
+++ src/ch/epfl/javions/Crc24.java
@@ -3,8 +3,8 @@ package ch.epfl.javions;
 public final class Crc24 {
     public static final int GENERATOR = 0xff_f4_09;
 
-    private static final int MASK_BIT_24 = (1 << 24);
-    private static final int MASK_LSBS_24 = 0xFF_FF_FF;
+    private static final int CRC_BITS = 24;
+    private static final int TOP_BYTE_START_BIT = CRC_BITS - Byte.SIZE;
 
     private final int[] table;
 
@@ -13,21 +13,25 @@ public final class Crc24 {
     }
 
     private static int[] buildTable(int generator) {
-        var table = new int[0x100];
+        var table = new int[1 << Byte.SIZE];
         for (var b = 0; b < table.length; b += 1) {
-            var crc = b << 16;
-            for (var i = 0; i < 8; i += 1) {
+            var crc = b << TOP_BYTE_START_BIT;
+            for (var i = 0; i < Byte.SIZE; i += 1) {
                 crc <<= 1;
-                if ((crc & MASK_BIT_24) != 0) crc ^= generator;
+                if (Bits.testBit(crc, CRC_BITS)) crc ^= generator;
             }
-            table[b] = crc & MASK_LSBS_24;
+            // Keeping only the low 24 bits is not strictly necessary, but cleaner.
+            table[b] = Bits.extractUInt(crc, 0, CRC_BITS);
         }
         return table;
     }
 
     public int crc(byte[] bytes) {
         var crc = 0;
-        for (var b : bytes) crc = (crc << 8) ^ table[((crc >> 16) ^ b) & 0xFF];
-        return crc & MASK_LSBS_24;
+        for (var b : bytes) {
+            var topByte = Bits.extractUInt(crc, TOP_BYTE_START_BIT, Byte.SIZE);
+            crc = (crc << Byte.SIZE) ^ table[topByte ^ Byte.toUnsignedInt(b)];
+        }
+        return Bits.extractUInt(crc, 0, CRC_BITS);
     }
 }
