12125bd5777d9f5460865cd1c375ec0100a18979
Michel Schinz <michel@schi.nz>
2023-02-02 06:44:18 +0100

    Rename lon/lat to (long/lat)itude in WebMercator

--- src/ch/epfl/javions/WebMercator.java
+++ src/ch/epfl/javions/WebMercator.java
@@ -7,13 +7,13 @@ import static java.lang.Math.tan;
 public final class WebMercator {
     private WebMercator() {}
 
-    public static double x(int zoomLevel, double lon) {
-        var x = 0.5 + lon * (1d / Units.Angle.TURN);
+    public static double x(int zoomLevel, double longitude) {
+        var x = 0.5 + longitude * (1d / Units.Angle.TURN);
         return atZoomLevel(zoomLevel, x);
     }
 
-    public static double y(int zoomLevel, double lat) {
-        var y = 0.5 - asinh(tan(lat)) * (1d / Units.Angle.TURN);
+    public static double y(int zoomLevel, double latitude) {
+        var y = 0.5 - asinh(tan(latitude)) * (1d / Units.Angle.TURN);
         return atZoomLevel(zoomLevel, y);
     }
 
