50c4d110c501ba030ffcf0befbaab96c9a00499a
Michel Schinz <michel@schi.nz>
2023-02-14 13:22:17 +0100

    Remove left-over debugging code from CallSign

--- src/ch/epfl/javions/adsb/CallSign.java
+++ src/ch/epfl/javions/adsb/CallSign.java
@@ -8,8 +8,6 @@ public record CallSign(String string) {
     private static final Pattern PATTERN = Pattern.compile("[A-Z0-9 ]{0,8}");
 
     public CallSign {
-        if (!PATTERN.matcher(string).matches())
-            System.out.printf("invalid call sign: %s\n", string);
         Preconditions.checkArgument(PATTERN.matcher(string).matches());
     }
 }
