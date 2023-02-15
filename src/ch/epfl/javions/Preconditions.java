e6d144f933df45da52df33567f1076a73b9e4992
Michel Schinz <michel@schi.nz>
2023-02-15 13:59:20 +0100

    Make Preconditions non-instantiable

--- src/ch/epfl/javions/Preconditions.java
+++ src/ch/epfl/javions/Preconditions.java
@@ -1,6 +1,8 @@
 package ch.epfl.javions;
 
 public final class Preconditions {
+    private Preconditions() {}
+
     public static void checkArgument(boolean mustBeTrue) {
         if (!mustBeTrue) throw new IllegalArgumentException();
     }
