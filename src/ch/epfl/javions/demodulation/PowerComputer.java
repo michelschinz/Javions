7aab49eee3570489bbf5adf019fbf744c0b75e82
Michel Schinz <michel@schi.nz>
2023-02-25 21:35:25 +0100

    Merge I/Q windows into a single one

--- src/ch/epfl/javions/demodulation/PowerComputer.java
+++ src/ch/epfl/javions/demodulation/PowerComputer.java
@@ -6,25 +6,23 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public final class PowerComputer {
-    private static final int FILTER_SIZE = 4;
+    private static final int FILTER_SIZE = 8;
 
     private final int batchSize;
     private final short[] samples;
     private final SamplesDecoder samplesDecoder;
 
-    private final int[] iWindow;
-    private final int[] qWindow;
+    private final int[] window;
     private int iSum, qSum;
 
-    // batchSize must be a multiple of FILTER_SIZE (4)
+    // batchSize must be a multiple of FILTER_SIZE (8)
     public PowerComputer(InputStream stream, int batchSize) {
         Preconditions.checkArgument(batchSize > 0 && batchSize % FILTER_SIZE == 0);
 
         this.batchSize = batchSize;
         this.samples = new short[batchSize * 2];
         this.samplesDecoder = new SamplesDecoder(stream, samples.length);
-        this.iWindow = new int[FILTER_SIZE];
-        this.qWindow = new int[FILTER_SIZE];
+        this.window = new int[FILTER_SIZE];
     }
 
     public int readBatch(int[] batch) throws IOException {
@@ -35,12 +33,12 @@ public final class PowerComputer {
         var powerI = 0;
         var sampleI = 0;
         while (sampleI < samplesRead) {
-            iSum += sampleDelta(iWindow, powerI % FILTER_SIZE, samples[sampleI++]);
-            qSum -= sampleDelta(qWindow, powerI % FILTER_SIZE, samples[sampleI++]);
+            iSum += sampleDelta(window, sampleI % FILTER_SIZE, samples[sampleI++]);
+            qSum -= sampleDelta(window, sampleI % FILTER_SIZE, samples[sampleI++]);
             batch[powerI++] = power(iSum, qSum);
 
-            iSum -= sampleDelta(iWindow, powerI % FILTER_SIZE, samples[sampleI++]);
-            qSum += sampleDelta(qWindow, powerI % FILTER_SIZE, samples[sampleI++]);
+            iSum -= sampleDelta(window, sampleI % FILTER_SIZE, samples[sampleI++]);
+            qSum += sampleDelta(window, sampleI % FILTER_SIZE, samples[sampleI++]);
             batch[powerI++] = power(iSum, qSum);
         }
 
