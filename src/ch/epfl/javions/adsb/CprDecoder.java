a8e295aa4ed16c61d2409240756e7d57085fd5f7
Michel Schinz <michel@schi.nz>
2023-03-10 12:19:47 +0100

    Use 0/1 instead of E/O in CprDecoder

--- src/ch/epfl/javions/adsb/CprDecoder.java
+++ src/ch/epfl/javions/adsb/CprDecoder.java
@@ -8,47 +8,43 @@ import static java.lang.Math.*;
 public final class CprDecoder {
     private static final int CPR_BITS = 17;
 
-    private static final int LAT_ZONES_E = 60;
-    private static final int LAT_ZONES_O = LAT_ZONES_E - 1;
+    private static final int LAT_ZONES_0 = 60;
+    private static final int LAT_ZONES_1 = LAT_ZONES_0 - 1;
 
-    private static final double D_LAT_E = 1d / LAT_ZONES_E;
-    private static final double D_LAT_O = 1d / LAT_ZONES_O;
+    private static final double D_LAT_0_TURN = 1d / LAT_ZONES_0;
+    private static final double D_LAT_1_TURN = 1d / LAT_ZONES_1;
 
-    private static final double LON_ZONES_NUMERATOR = 1 - cos(D_LAT_E * Units.Angle.TURN);
+    private static final double LON_ZONES_NUMERATOR = 1 - cos(D_LAT_0_TURN * Units.Angle.TURN);
 
-    private static int lonZones(double latitude) {
-        var cosLat = cos(latitude * Units.Angle.TURN);
+    private static int lonZones(double latTurn) {
+        var cosLat = cos(latTurn * Units.Angle.TURN);
         var nl = floor(Units.Angle.TURN / acos(1 - LON_ZONES_NUMERATOR / (cosLat * cosLat)));
         return Double.isNaN(nl) ? 1 : (int) nl;
     }
 
-    public static GeoPos decodePosition(int lonCprE,
-                                        int latCprE,
-                                        int lonCprO,
-                                        int latCprO,
-                                        boolean mostRecentIsE) {
-        var latZIn = (int) rint(cprToDouble(LAT_ZONES_O * latCprE - LAT_ZONES_E * latCprO));
-        var latE = D_LAT_E * (normalizeZoneIndex(latZIn, LAT_ZONES_E) + cprToDouble(latCprE));
-        var latO = D_LAT_O * (normalizeZoneIndex(latZIn, LAT_ZONES_O) + cprToDouble(latCprO));
+    public static GeoPos decodePosition(int x0, int y0, int x1, int y1, boolean mostRecentIs0) {
+        var latZIn = (int) rint(cprToDouble(LAT_ZONES_1 * y0 - LAT_ZONES_0 * y1));
+        var lat0Turn = D_LAT_0_TURN * (normalizeZoneIndex(latZIn, LAT_ZONES_0) + cprToDouble(y0));
+        var lat1Turn = D_LAT_1_TURN * (normalizeZoneIndex(latZIn, LAT_ZONES_1) + cprToDouble(y1));
 
-        var lonZonesE = lonZones(latE);
-        if (lonZonesE != lonZones(latO)) return null;
+        var lonZones0 = lonZones(lat0Turn);
+        if (lonZones0 != lonZones(lat1Turn)) return null;
 
-        if (lonZonesE == 1) {
-            return mostRecentIsE
-                    ? geoPos(cprToDouble(lonCprE), latE)
-                    : geoPos(cprToDouble(lonCprO), latO);
+        if (lonZones0 == 1) {
+            return mostRecentIs0
+                    ? geoPos(cprToDouble(x0), lat0Turn)
+                    : geoPos(cprToDouble(x1), lat1Turn);
         } else {
-            var lonZonesO = lonZonesE - 1;
-            var lonZIn = (int) rint(cprToDouble(lonZonesO * lonCprE - lonZonesE * lonCprO));
-            return mostRecentIsE
-                    ? geoPos(normalizeZoneIndex(lonZIn, lonZonesE) + cprToDouble(lonCprE) / lonZonesE, latE)
-                    : geoPos(normalizeZoneIndex(lonZIn, lonZonesO) + cprToDouble(lonCprO) / lonZonesO, latO);
+            var lonZones1 = lonZones0 - 1;
+            var lonZIn = (int) rint(cprToDouble(lonZones1 * x0 - lonZones0 * x1));
+            return mostRecentIs0
+                    ? geoPos(normalizeZoneIndex(lonZIn, lonZones0) + cprToDouble(x0) / lonZones0, lat0Turn)
+                    : geoPos(normalizeZoneIndex(lonZIn, lonZones1) + cprToDouble(x1) / lonZones1, lat1Turn);
         }
     }
 
-    private static double cprToDouble(int x) {
-        return scalb((double) x, -CPR_BITS);
+    private static double cprToDouble(int cpr) {
+        return scalb((double) cpr, -CPR_BITS);
     }
 
     private static int normalizeZoneIndex(int zIn, int zonesCount) {
