47057ebb93e87f6a6f240a4a86569a6c1638fd22
Michel Schinz <michel@schi.nz>
2023-03-12 15:46:25 +0100

    Clean-up AircraftIdentificationMessage

--- src/ch/epfl/javions/adsb/AircraftIdentificationMessage.java
+++ src/ch/epfl/javions/adsb/AircraftIdentificationMessage.java
@@ -8,9 +8,12 @@ public record AircraftIdentificationMessage(long timeStampNs,
                                             int category,
                                             CallSign callSign) implements Message {
     private static final int CALL_SIGN_LENGTH = 8;
-    private static final int CALL_SIGN_CHAR_BITS = 6;
+    private static final int CALL_SIGN_CHAR_SIZE = 6;
     private static final String CALL_SIGN_ALPHABET =
             "?ABCDEFGHIJKLMNOPQRSTUVWXYZ????? ???????????????0123456789??????";
+
+    private static final int CATEGORY_START = CALL_SIGN_LENGTH * CALL_SIGN_CHAR_SIZE;
+    private static final int CATEGORY_SIZE = 3;
 
     public static AircraftIdentificationMessage of(RawMessage rawMessage) {
         var payload = rawMessage.payload();
@@ -25,15 +28,15 @@ public record AircraftIdentificationMessage(long timeStampNs,
     }
 
     private static int category(long payload) {
-        var category = Bits.extractUInt(payload, 48, 3);
+        var category = Bits.extractUInt(payload, CATEGORY_START, CATEGORY_SIZE);
         return (0xE - RawMessage.typeCode(payload)) << 4 | category;
     }
 
     private static CallSign callSign(long payload) {
         var callSignChars = new char[CALL_SIGN_LENGTH];
         for (var i = 0; i < CALL_SIGN_LENGTH; i += 1) {
-            var startBitI = (CALL_SIGN_LENGTH - 1 - i) * CALL_SIGN_CHAR_BITS;
-            var n = Bits.extractUInt(payload, startBitI, CALL_SIGN_CHAR_BITS);
+            var startBitI = (CALL_SIGN_LENGTH - 1 - i) * CALL_SIGN_CHAR_SIZE;
+            var n = Bits.extractUInt(payload, startBitI, CALL_SIGN_CHAR_SIZE);
             callSignChars[i] = CALL_SIGN_ALPHABET.charAt(n);
         }
         var callSign = new String(callSignChars).trim();
