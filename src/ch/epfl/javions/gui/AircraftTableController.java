f3f9b8f0ac56d051c9b2d823689c3791fd21de29
Michel Schinz <michel@schi.nz>
2023-04-26 09:18:57 +0200

    Clean-up AircraftTableController

--- src/ch/epfl/javions/gui/AircraftTableController.java
+++ src/ch/epfl/javions/gui/AircraftTableController.java
@@ -4,10 +4,9 @@ import ch.epfl.javions.GeoPos;
 import ch.epfl.javions.Units;
 import ch.epfl.javions.adsb.CallSign;
 import ch.epfl.javions.aircraft.AircraftData;
-import ch.epfl.javions.aircraft.IcaoAddress;
 import javafx.beans.binding.Bindings;
+import javafx.beans.binding.DoubleExpression;
 import javafx.beans.property.ObjectProperty;
-import javafx.beans.value.ObservableDoubleValue;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableSet;
 import javafx.collections.SetChangeListener;
@@ -20,7 +19,6 @@ import javafx.scene.layout.Pane;
 
 import java.text.NumberFormat;
 import java.text.ParseException;
-import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -39,38 +37,30 @@ public final class AircraftTableController {
         this.tableView = tableView;
         this.pane = pane;
 
-        tableView.setOnMouseClicked(e -> {
-            if (e.getClickCount() == 2
-                    && MouseButton.PRIMARY.equals(e.getButton())
-                    && doubleClickConsumer != null)
-                doubleClickConsumer.accept(tableView.getSelectionModel().getSelectedItem());
-        });
-
-        installListeners(aircraftStates);
-        selectedAddressProperty.addListener((p, o, n) -> {
-            if (!Objects.equals(tableView.getSelectionModel().getSelectedItem(), n))
-                tableView.scrollTo(n);
-            tableView.getSelectionModel().select(n);
-        });
-        tableView.getSelectionModel().selectedItemProperty().addListener((p, o, n) -> selectedAddressProperty.set(n));
+        installHandlers();
+        installListeners(aircraftStates, selectedAddressProperty);
     }
 
     private static TableView<ObservableAircraftState> createTableView() {
-        var columns = List.of(
+        var tableView = new TableView<ObservableAircraftState>();
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_FLEX_LAST_COLUMN);
+        tableView.setTableMenuButtonVisible(true);
+        //noinspection unchecked
+        tableView.getColumns().setAll(
                 newStringColumn("OACI",
-                        s -> makeObservable(s.address()).map(IcaoAddress::string)),
+                        s -> constantObservable(s.address().string())),
                 newStringColumn("Indicatif",
                         s -> s.callSignProperty().map(CallSign::string)),
                 newStringColumn("Immatriculation",
-                        s -> makeObservable(s.aircraftData()).map(d -> d.registration().string())),
+                        s -> constantObservable(s.aircraftData()).map(d -> d.registration().string())),
                 newStringColumn("Modèle",
-                        s -> makeObservable(s.aircraftData()).map(AircraftData::model)),
+                        s -> constantObservable(s.aircraftData()).map(AircraftData::model)),
                 newDoubleColumn("Longitude (°)",
-                        s -> convert(s.positionProperty().map(GeoPos::longitude)),
+                        s -> asDoubleExpression(s.positionProperty().map(GeoPos::longitude)),
                         Units.Angle.DEGREE,
                         4),
                 newDoubleColumn("Latitude (°)",
-                        s -> convert(s.positionProperty().map(GeoPos::latitude)),
+                        s -> asDoubleExpression(s.positionProperty().map(GeoPos::latitude)),
                         Units.Angle.DEGREE,
                         4),
                 newDoubleColumn("Altitude (m)",
@@ -81,11 +71,6 @@ public final class AircraftTableController {
                         ObservableAircraftState::velocityProperty,
                         Units.Speed.KILOMETER_PER_HOUR,
                         0));
-
-        var tableView = new TableView<ObservableAircraftState>();
-        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_FLEX_LAST_COLUMN);
-        tableView.setTableMenuButtonVisible(true);
-        tableView.getColumns().setAll(columns);
         return tableView;
     }
 
@@ -99,7 +84,7 @@ public final class AircraftTableController {
 
     private static TableColumn<ObservableAircraftState, String> newDoubleColumn(
             String title,
-            Function<ObservableAircraftState, ObservableDoubleValue> propertyExtractor,
+            Function<ObservableAircraftState, DoubleExpression> propertyExtractor,
             double unit,
             int fractionDigits) {
         var column = new TableColumn<ObservableAircraftState, String>(title);
@@ -119,7 +104,7 @@ public final class AircraftTableController {
 
         column.setCellValueFactory(f -> {
             var p = propertyExtractor.apply(f.getValue());
-            return Bindings.when(Bindings.greaterThan(p, Double.NEGATIVE_INFINITY))
+            return Bindings.when(p.greaterThan(Double.NEGATIVE_INFINITY))
                     .then(formatter.format(Units.convertTo(p.get(), unit)))
                     .otherwise("");
         });
@@ -132,11 +117,11 @@ public final class AircraftTableController {
         return column;
     }
 
-    private static <T> ObservableValue<T> makeObservable(T value) {
+    private static <T> ObservableValue<T> constantObservable(T value) {
         return Bindings.createObjectBinding(() -> value);
     }
 
-    private static ObservableDoubleValue convert(ObservableValue<Double> observableValue) {
+    private static DoubleExpression asDoubleExpression(ObservableValue<Double> observableValue) {
         return Bindings.createDoubleBinding(observableValue::getValue, observableValue);
     }
 
@@ -148,12 +133,29 @@ public final class AircraftTableController {
         }
     }
 
-    private void installListeners(ObservableSet<ObservableAircraftState> aircraftStates) {
+    private void installHandlers() {
+        tableView.setOnMouseClicked(e -> {
+            if (e.getClickCount() == 2
+                && MouseButton.PRIMARY.equals(e.getButton())
+                && doubleClickConsumer != null)
+                doubleClickConsumer.accept(tableView.getSelectionModel().getSelectedItem());
+        });
+    }
+
+    private void installListeners(ObservableSet<ObservableAircraftState> aircraftStates,
+                                  ObjectProperty<ObservableAircraftState> selectedAddressProperty) {
         aircraftStates.addListener((SetChangeListener<ObservableAircraftState>) c -> {
             var tableItems = tableView.getItems();
             if (c.wasRemoved()) tableItems.remove(c.getElementRemoved());
             if (c.wasAdded()) tableItems.add(c.getElementAdded());
         });
+
+        selectedAddressProperty.addListener((p, o, n) -> {
+            if (!Objects.equals(tableView.getSelectionModel().getSelectedItem(), n))
+                tableView.scrollTo(n);
+            tableView.getSelectionModel().select(n);
+        });
+        tableView.getSelectionModel().selectedItemProperty().addListener((p, o, n) -> selectedAddressProperty.set(n));
     }
 
     public Node pane() {
